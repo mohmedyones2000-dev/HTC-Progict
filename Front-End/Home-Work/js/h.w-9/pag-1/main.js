f7306fb33c5f5c2af544a258ddec771b1c8e3dc6
amam1112 <mohmedyones2000@gmail.com>
2026-06-22 13:06:47 +0300

    update files

---
+++ Front-End/Home-Work/js/h.w-9/pag-1/main.js
@@ -0,0 +1,39 @@
+let grades = [];
+
+for (let i = 0; i < 6; i++) {
+    let input = prompt(`الرجاء إدخال العلامة رقم ${i + 1}:`);
+    grades.push(Number(input));
+}
+
+const container = document.getElementById('results-container');
+
+let totalSum = 0; 
+
+grades.forEach((grade, index) => {
+    
+    totalSum += grade; 
+    
+    let resultDiv = document.createElement('div');
+    resultDiv.classList.add('result-card');
+    
+    if (grade >= 60) {
+    resultDiv.textContent = `الطالب ${index + 1}: ناجح (العلامة: ${grade})`;
+    resultDiv.classList.add('success');
+} else {
+    resultDiv.textContent = `الطالب ${index + 1}: راسب (العلامة: ${grade})`;
+    resultDiv.classList.add('fail'); 
+}
+    
+    container.appendChild(resultDiv);
+});
+
+let average = totalSum / grades.length;
+
+let averageDiv = document.createElement('h3');
+averageDiv.style.marginTop = "25px";
+averageDiv.style.padding = "10px";
+averageDiv.style.borderTop = "2px dashed #ccc";
+
+averageDiv.textContent = `متوسط علامات الطلاب هو: ${average.toFixed(2)}`;
+
+container.appendChild(averageDiv);
